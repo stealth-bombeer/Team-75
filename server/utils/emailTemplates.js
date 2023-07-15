71bc29b72b9b1177d8acf983a97370ad820c279c
Swayam Desai <swayamdesai62@gmail.com>
2023-07-15 09:47:07 +0530

    link added

--- server/utils/emailTemplates.js
+++ server/utils/emailTemplates.js
@@ -3,8 +3,9 @@ let resetPassword = (email, token) => {
       from: "noreply@gmail.com",
       to: email,
       subject: "Password reset for " + email,
-      text:
-        "Password Reset Link: " + "localhost:3000/" + "resetPassword/" + token,
+      // text:
+      //   "Password Reset Link: " + "localhost:3000/" + "resetPassword/" + token,
+       html: `<p>Password Reset Link: <a href="http://localhost:3000/resetPassword/${token}" style="color: blue;">Click here</a></p>`,
     };
     return emailTemplate;
   };
